dfeccb8af999e072468720d680cfd17fd6c4fddd
Charo <diridolloumarwan@gmail.com>
2016-09-14 16:47:25 +0200

    finalisation inscription

--- validation.php
+++ validation.php
@@ -2,17 +2,54 @@
 require('config/config.php');
 require('model/functions.fn.php');
 session_start();
+
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
-	// TODO
-	$username = htmlspecialchars($_POST["username"]);
-	$email = htmlspecialchars($_POST["email"]);
-  	$password = htmlspecialchars($_POST["password"]);
+   
+    $username = htmlspecialchars($_POST["username"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
 
-  	userRegistration($db, $username, $email, $password);
-  	$_SESSION['message'] = 'inscription réussie';
-  	header('Location: register.php');
-}else{ 
+    if((isUsernameAvailable($db, $username)==true) && (isEmailAvailable($db, $email)==true)){
+    userRegistration( $db, $username, $email, $password);
+		header('Location: dashboard.php');
+
+    }
+
+    elseif((isUsernameAvailable($db, $username)==true) && (isEmailAvailable($db, $email)==false)){
+        $_SESSION["message"] = 'Erreur: email indisponible';
+        header('Location: register.php');
+
+    }
+
+    elseif((isUsernameAvailable($db, $username)==false) && (isEmailAvailable($db, $email)==true)){
+        $_SESSION["message"] = 'Erreur: username indisponible';
+        header('Location: register.php');
+
+    }
+    elseif((isUsernameAvailable($db, $username)==false) && (isEmailAvailable($db, $email)==false)){
+        $_SESSION["message"] = 'Erreur: username et email indisponible';
+        header('Location: register.php');
+
+    }
+
+
+
+
+    else{
+        echo" Error : this member already exists";
+        header('Location: register.php');
+
+    };
+
+
+}
+
+
+
+else{
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
+
 }
+?>
