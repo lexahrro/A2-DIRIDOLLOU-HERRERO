9ee0f06f76f1be55a14a4bed76e7415e2b6f2563
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-09-14 16:29:25 +0200

    validation update

--- validation.php
+++ validation.php
@@ -5,10 +5,23 @@ session_start();
 
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+	
+	$email = htmlspecialchars($_POST["email"]);
+  	$password = htmlspecialchars($_POST["password"]);
 
-	// TODO
+	userConnection($db, $email, $password);
+	if (userConnection($db, $email, $password) == true) {
+		header('Location: dashboard.php');
+	}
+ 	else{
+		$error = "Mauvais identifiants";
+	}
 
-}else{ 
+}else {
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
+}
+
+else{
+	$error = 'Champs requis !';
 }
